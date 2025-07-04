c208787c92f05f33da7f89f61321e92c9ba3aa67
evakichikun <evakichikun@gmail.com>
2025-07-05 00:43:58 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,16 +2,16 @@ import { CronJob } from "cron";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { parseCommandLineArgs } from './parseCommandLineArgs';
+import { parseCommandLineArgs } from './utils/parseCommandLineArgs';
 
 import {
   getRakutenRankingDataByGenre,
   getRakutenRankingDataByKeyword,
-} from "./getRakutenRankingData";
+} from "./services/getRakutenRankingData";
 
-import getGenreIdsByTime from "./getGenreIdsByTime";
-import getNumberToday from "./lib/getNumberToday";
-import postRakutenRoom from "./postRakutenRoom"; // ✅ defaultインポートに修正
+import getGenreIdsByTime from "./utils/getGenreIdsByTime";
+import getNumberToday from "./utils/getNumberToday";
+import postRakutenRoom from "./services/postRakutenRoom";
 
 async function runJob() {
   const today = new Date();
@@ -29,26 +29,3 @@ async function runJob() {
 }
 
 async function main(
-  getRakutenRankingData: (genreOrKeyword: string, numberToday: number) => any,
-  genreOrKeyword: string
-) {
-  const numberToday = getNumberToday();
-  const elements = await getRakutenRankingData(genreOrKeyword, numberToday);
-  await postRakutenRoom(elements); // ✅ 引数は1つ
-  console.log("End job:" + new Date().toLocaleString());
-}
-
-const args = process.argv.slice(2);
-const options = parseCommandLineArgs(args);
-
-if (options.genre) {
-  main(getRakutenRankingDataByGenre, options.genre);
-} else if (options.keyword) {
-  main(getRakutenRankingDataByKeyword, options.keyword);
-} else {
-  const job = new CronJob("0 0 * * * *", () => {
-    console.log("Start job:" + new Date().toLocaleString());
-    runJob();
-  });
-  job.start();
-}
